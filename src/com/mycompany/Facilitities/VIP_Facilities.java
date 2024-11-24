e4f26ea97af1b07a5c2978216b204ef7076b88ba
Syarif <134999646+mendicore@users.noreply.github.com>
2024-11-24 16:26:03 +0700

    Create VIP_Facilities.java

---
+++ src/com/mycompany/Facilitities/VIP_Facilities.java
@@ -0,0 +1,7 @@
+package com.mycompany.Facilities;
+
+public interface VIP_Facilities {
+    public void assignMaid();
+    public void buySpaPackage();
+    public void grantExclusiveEventAccess();
+}
