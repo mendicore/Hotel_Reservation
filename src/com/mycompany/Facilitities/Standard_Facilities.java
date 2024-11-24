3d4e78c79528f409005e4dd41e5b188e65e83255
Syarif <134999646+mendicore@users.noreply.github.com>
2024-11-24 16:25:19 +0700

    Create Standard_Facilities.java

---
+++ src/com/mycompany/Facilitities/Standard_Facilities.java
@@ -0,0 +1,9 @@
+package com.mycompany.Facilities;
+
+public interface Standard_Facilities {
+    public void Discount();
+    public void addExtraBed();
+    public void addMeal();
+    public void addWifi();
+    
+}
